0b8242d94225a3d6426ab6ec143d6015b9b9a210
imseohyeon <forestlight749@gmail.com>
2025-04-02 17:16:08 +0900

    (0402) array 코드 추가 및 수정

--- c_js/app/e_array.js
+++ c_js/app/e_array.js
@@ -1,4 +1,27 @@
+let top1 = {
+  title : '소년이 온다',
+  awards : ['노벨문학상', '이상문학상'],
+  category : {depth1: '문학', depth2: '소설'},
+  'categoryCode':12,
+}
+
+let top2 = {
+  title : '당신의 이름을 지어다가 며칠은 먹었다',
+  awards : ['젊은 예술가상'],
+  category : {depth1: '문학', depth2: '시'},
+  'categoryCode':12,
+}
+
+let top3 = {
+  title : 'Object',
+  awards : [],
+  category : {depth1: '기술', depth2: 'IT'},
+  'categoryCode':15,
+}
+
+
 // C
+// 배열의 가장 뒤에 요소를 추가
 const studyPush = () => {
   const arr = ['a', 'b', 'c', 'd', 100];
   arr.push('e');
@@ -51,35 +74,12 @@ const studyDelete = () => {
   console.log(arr);
 }
 
-studyDelete();
-
-let top1 = {
-  title : '소년이 온다',
-  awards : ['노벨문학상', '이상문학상'],
-  category : {depth1: '문학', depth2: '소설'},
-  'categoryCode':12,
-}
-
-let top2 = {
-  title : '당신의 이름을 지어다가 며칠은 먹었다',
-  awards : ['젊은 예술가상'],
-  category : {depth1: '문학', depth2: '시'},
-  'categoryCode':12,
-}
-
-let top3 = {
-  title : 'Object',
-  awards : [],
-  category : {depth1: '기술', depth2: 'IT'},
-  'categoryCode':15,
-}
-
 // map filter reduce
 const studyMap = () => {
-  const arr = [1, 2, 3, 4, 5, 6];
+  const arr = [1,2,3,4,5,6];
 
-  // arr의 각 요소에 10을 곱한 배열이 필요하다.
-  const res = arr.map(e => e * 10);
+  // arr 의 각 요소에 10을 곱한 배열이 필요하다.
+  const res = arr.map( e => e * 10);
   console.log(res);
 
   const books = [top1, top2, top3];
@@ -90,7 +90,7 @@ const studyMap = () => {
 studyMap();
 
 const studyFilter = () => {
-  const arr = [1, 2, 3, 4, 5, 6];
+  const arr = [1,2,3,4,5,6];
 
   const even = arr.filter(e => e % 2 === 0);
   console.log(even);
@@ -100,37 +100,41 @@ const studyFilter = () => {
 }
 
 studyFilter();
-
 const studyReduce = () => {
-  const arr = [1, 2, 3, 4, 5, 6];
+  const arr = [1,2,3,4,5,6];
 
-  const oddSum = arr.reduce( (acc, cur) => {
+  const oddSum = arr.reduce((acc, cur) => {
     if(cur % 2 === 1){
       acc += cur;
     }
     return acc;
-  }, 0)
+    } , 0)
 
   console.log(oddSum);
 }
-
 studyReduce();
 
-// arr 의 각 요소에 10을 곱한 배열이 필요하다
+// arr 의 각 요소에 10을 곱한 배열이 필요하다.
 // reduce 를 사용해 구현
 const quizReduce = () => {
-  const arr = [1, 2, 3, 4, 5, 6];
-  const res = arr.reduce((acc, cur) => {
+  const arr = [1,2,3,4,5,6];
+  const res = arr.reduce((acc , cur) => {
     acc.push(cur * 10);
     return acc;
   }, [])
 
   console.log(res);
 }
-
 quizReduce();
 
-// 유사배열객체
+const studyJoin = () => {
+  const arr = ['a', 'b', 'c', 'd'];
+  console.dir(arr.join('****'));
+}
+
+studyJoin();
+
+// 유사배열객채
 // length 속성이 있다.
 // 객체의 속성이 index 이다.
 const similarArray = {
@@ -140,14 +144,9 @@ const similarArray = {
   length: 3
 }
 
-const studysimilarArray = () => {
-  // console.log(similarArray[0]);
-
-  //similarArray.forEach(e => console.log(e)); // 배열이 forEach 불가능
-
-  // 유사배열객체를 배열로 만들기 위한 from()
+const studySimilarArray = () => {
   const arr = Array.from(similarArray);
   arr.forEach(e => console.log(e));
 }
 
-studysimilarArray();
+studySimilarArray();
