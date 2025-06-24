7fe74b8a81c46e2f58f7745cc451b6778f5d5655
imseohyeon <forestlight749@gmail.com>
2025-06-25 02:32:31 +0900

    (0624) refactor: Game 컴포넌트를 App에서 분리하여 Game.js로 이동

--- d_react/grepp-react/src/App.js
+++ d_react/grepp-react/src/App.js
@@ -1,113 +1,25 @@
-// 객체를 매개변수로 받아서 각 변수에 구조분해하여 할당 (비구조화할당)
-import {useState} from "react";
+import Game from "./pages/Game";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./pages/common/Layout";
+import Login from "./pages/Login";
 
-function Square({value, onSquareClick }) {
-  function handleClick(){
-    //setValue('X')
-  }
+export default function App(){
 
-  return <button className="square" onClick={onSquareClick}>{value}</button>
-}
-
-export default function Game(){
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[currentMove]
-  const xIsNext = currentMove % 2 === 0
-
-  function handlePlay(nextSquares){
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(idx){
-    setCurrentMove(idx)
-  }
-
-  const moves = history.map((squares, idx) => {
-    let description;
-    description = 'Go to move #' + idx;
+  const Index = () => {
     return (
-        <li key={idx}>
-          <button onClick={() => jumpTo(idx)}>{description}</button>
-        </li>
-    );
-  })
+        <>
+          <h2>Hello React</h2>
+        </>
+    )
+  }
 
   return (
-      <div className="game">
-        <div className="game-board">
-          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-        </div>
-        <div className="game-info">
-          <ol>{moves}</ol>
-        </div>
-      </div>
-  );
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Index/>} />
+          <Route path="/game" element={<Game/>} />
+          <Route path="/login" element={<Login/>} />
+        </Route>
+      </Routes>
+  )
 }
-
-function Board({xIsNext, squares, onPlay}) {
-  const winner = calculateWinner(squares);
-  let status;
-  if(winner){
-    status = winner + " is win!!"
-  }else{
-    status = "next player is " + (xIsNext ? 'X' : 'O')
-  }
-
-  function handleClick(i) {
-    const nextSquares = squares.slice();
-    if(nextSquares[i] || calculateWinner(squares)) return;
-    if(xIsNext){
-      nextSquares[i] = "X";
-    }else{
-      nextSquares[i] = "O";
-    }
-
-    onPlay(nextSquares)
-  }
-
-  // jsx 문법
-  return (
-      <>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-        </div>
-        <div className="board-row">
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-        </div>
-        <div className="board-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-        </div>
-      </>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
